e718d8bd6429e8b891ae71de497efc6b56bad78d
Josue Palafox <jpalafoxe90@gmail.com>
2024-03-14 01:36:58 -0700

    Full screen progress indicator terminado

--- lib/presentation/widgets/shared/full_screen_loader.dart
+++ lib/presentation/widgets/shared/full_screen_loader.dart
@@ -3,8 +3,42 @@ import 'package:flutter/material.dart';
 class FullScreenLoader extends StatelessWidget {
   const FullScreenLoader({super.key});
 
+  Stream<String> getLoadingMessages(){
+
+    final messages = <String>[
+    "Cargando películas",
+    "Comprando palomitas",
+    "Cargando populares",
+    "Ya casi...",
+    "Se está tardando más de lo esperado :("
+    ];
+
+    return Stream.periodic(const Duration(milliseconds: 1800), (step){
+      return messages[step];
+    }).take(messages.length);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Espere por favor"),
+          const SizedBox(height: 12),
+          const CircularProgressIndicator(strokeWidth: 3,),
+          const SizedBox(height: 12),
+
+          StreamBuilder(
+            stream: getLoadingMessages(), 
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Text("Cargando");
+
+              return Text(snapshot.data!);
+            },
+          )
+        ]
+      ),
+    );
   }
 }
