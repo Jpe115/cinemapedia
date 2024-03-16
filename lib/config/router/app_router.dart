6763d7ab7add557d7284737be993061e424122b1
Josue Palafox <jpalafoxe90@gmail.com>
2024-03-16 15:42:30 -0700

    Ruta para movie creada

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -10,7 +10,17 @@ final appRouter = GoRouter(
       path: "/",
       name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
-    )
+    ),
+
+    GoRoute(
+      path: "/movie/:id",
+      name: MovieScreen.name,
+      builder: (context, state) {
+        final movieID = state.pathParameters["id"] ?? "no-id";
+
+        return MovieScreen(movieId: movieID);
+      },
+    ),
 
   ]
 );
