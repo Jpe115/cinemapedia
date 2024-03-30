b69870bc647305c586ec67f9cf0f632a43848650
jpe <jpalafoxe90@gmail.com>
2024-03-30 14:30:36 -0700

    Añadida función de search movie

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -84,5 +84,17 @@ class MovieDBDatasource extends MoviesDatasource{
     final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
     return movie;
   }
+  
+  @override
+  Future<List<Movie>> searchMovies(String query) async{
+    
+    final response = await dio.get("/search/movie", 
+      queryParameters: {
+        "query": query 
+      }
+    );
+    
+    return _jsonToMovies(response.data);
+  }
 
 }
