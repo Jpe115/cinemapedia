fc9a3d6d061f55c1d10d03813ce535f94092ea83
Josue Palafox <jpalafoxe90@gmail.com>
2024-04-03 15:21:28 -0700

    Cambio hacia StatefulShellRoute para mantener el estado de los tabs de GoRouter

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,28 +3,38 @@ import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:cinemapedia/presentation/views/views.dart';
 
-final appRouter = GoRouter(
-  initialLocation: "/",
-  routes: [
-
-    GoRoute(
-      path: "/",
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(childView: HomeView(),),
-      routes: [
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  // State-Preserving
+  StatefulShellRoute.indexedStack(
+    builder: (context, state, navigationShell) =>
+      HomeScreen(childView: navigationShell),
+    branches: [
+      StatefulShellBranch(routes: <RouteBase>[
         GoRoute(
-          path: "movie/:id",
-          name: MovieScreen.name,
+          path: '/',
           builder: (context, state) {
-            final movieID = state.pathParameters["id"] ?? "no-id";
-
-            return MovieScreen(movieId: movieID);
-      },
-    ),
-      ]
-    ),
-
-    
-
-  ]
-);
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
+                  return MovieScreen(movieId: movieId);
+                },
+              ),
+          ]
+        )
+      ]),
+      StatefulShellBranch(routes: <RouteBase>[
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        )
+      ]),
+    ]
+  ),
+]);
