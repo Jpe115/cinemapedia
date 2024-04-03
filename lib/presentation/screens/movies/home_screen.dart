848cec0f4e880ee8a0b98313e7eaf03c8832beaa
Josue Palafox <jpalafoxe90@gmail.com>
2024-04-02 21:45:05 -0700

    Home screen adaptado a la nueva manera de manejar las vistas y su cambio en app router correspondiente

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,158 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
-
 
 class HomeScreen extends StatelessWidget {
 
   static const name = "home-screen";
-  const HomeScreen({super.key});
+
+  final Widget childView;
+
+  const HomeScreen({
+    super.key, 
+    required this.childView
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
+    return Scaffold(
+      body: childView,
 
-      bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-
-
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-
-  @override
-  void initState() {
-    super.initState();
-    
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if(initialLoading) return const FullScreenLoader();
-
-    final colors = Theme.of(context).colorScheme;
-    final titleStyle = Theme.of(context).textTheme.titleLarge;
-
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-
-
-    return CustomScrollView(
-      slivers: [
-
-        SliverAppBar(
-          floating: true,
-          toolbarHeight: 55,
-          title: Row(
-            children: [
-              Icon(Icons.movie_outlined, color: colors.primary),
-              const SizedBox(width: 4,),
-              Text("Cinemapedia", style: titleStyle,)
-            ],
-          ),
-          actions: [
-            IconButton(
-              onPressed: (){
-
-                final searchQuery = ref.read(searchQueryProvider); 
-                final searchedMovies = ref.read(searchedMoviesProvider);
-
-                showSearch<Movie?>(
-                  query: searchQuery,
-                  context: context, 
-                  delegate: SearchMovieDelegate(
-                    initialMovies: searchedMovies,
-                    searchMovies: ref.read(searchedMoviesProvider.notifier).searchMoviesByQuery
-                  )
-                ).then((movie) {
-                  if (movie == null) return;
-
-                  context.push("/movie/${movie.id}");
-                });
-              }, 
-              icon: const Icon(Icons.search_rounded)
-            )
-          ],
-          // flexibleSpace: FlexibleSpaceBar(
-          //   title: CustomAppBar(),
-          // ),
-        ),
-
-        SliverList(delegate: SliverChildBuilderDelegate(
-          (context, index){
-            return Column(
-              children: [
-                //const CustomAppBar(),
-
-                MoviesSlideshow(movies: slideShowMovies),
-
-                MovieHorizontalListview(
-                  movies: nowPlayingMovies,
-                  title: "En cines",
-                  subTitle: "Lunes 20",
-                  loadNextPage: () {
-                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-
-                MovieHorizontalListview(
-                  movies: upcomingMovies,
-                  title: "Próximamente",
-                  subTitle: "Este mes",
-                  loadNextPage: () {
-                    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-
-                MovieHorizontalListview(
-                  movies: popularMovies,
-                  title: "Populares",
-                  //subTitle: "De todos los tiempos",
-                  loadNextPage: () {
-                    ref.read(popularMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-
-                MovieHorizontalListview(
-                  movies: topRatedMovies,
-                  title: "Mejor calificadas",
-                  subTitle: "De todos los tiempos",
-                  loadNextPage: () {
-                    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-
-                const SizedBox(height: 10,)
-              ],
-            );    
-          },
-          childCount: 1
-        ))
-      ] 
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
