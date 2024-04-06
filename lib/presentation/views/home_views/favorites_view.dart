14c608ca6b4d6375914477144e6b0b52b5f46289
Josue Palafox <jpalafoxe90@gmail.com>
2024-04-06 01:26:35 -0700

    Creada pantalla favoritos

--- lib/presentation/views/home_views/favorites_view.dart
+++ lib/presentation/views/home_views/favorites_view.dart
@@ -1,17 +1,65 @@
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  bool isLastPage = false;
+  bool isLoading = false;
+
+  void loadNextPage() async{
+    if (isLoading || isLastPage) return;
+    isLoading = true;
+      
+    final movies = await ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+    isLoading = false;
+
+    if (movies.isEmpty) {
+      isLastPage = true;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    //loadNextPage();
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+    final colors = Theme.of(context).colorScheme;
+    final textStyle = Theme.of(context).textTheme;
+
+    if (favoriteMovies.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_border_sharp, size: 50, color: colors.primary,),
+            Text("Oh no D:", style: TextStyle(fontSize: textStyle.titleLarge!.fontSize, color: colors.primary)
+            ),
+            Text("Aún no tienes películas en favoritos", 
+              style: TextStyle(fontSize: textStyle.titleMedium!.fontSize, color: Colors.black54)
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Favorites View"),
-      ),
-      body: const Center(
-        child: Text("Favoritos"),
-      ),
+      body: MovieMasonry(movies: favoriteMovies, loadNextPage: loadNextPage,)
     );
   }
 }
